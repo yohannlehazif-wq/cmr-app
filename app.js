53a5fe2ec1b9471c21af8c4174b8f5d3d4236d0f
yohannlehazif-wq <yohannlehazif@gmail.com>
2026-04-04 17:29:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,48 +20,24 @@ function analyze() {
 
   if (isCMR) {
     html += `
-      <h3 style="color:red;">🔴 Produit CMR détecté</h3>
+      <h3 style="color:red;">🔴 PRODUIT CMR</h3>
       <p><b>${produit}</b></p>
       <p><b>Mentions :</b> ${selectedH.join(", ")}</p>
-
-      <h4>Pourquoi c’est dangereux :</h4>
-      <ul>
-        <li>Peut provoquer un cancer (H350)</li>
-        <li>Peut altérer l’ADN (H340)</li>
-        <li>Peut nuire à la fertilité / fœtus (H360)</li>
-      </ul>
-
-      <h4>Prévention :</h4>
-      <ul>
-        <li>Substitution si possible</li>
-        <li>Ventilation renforcée + aspiration</li>
-        <li>Masque A2P3 obligatoire</li>
-        <li>Gants nitrile</li>
-        <li>Lunettes enveloppantes</li>
-      </ul>
     `;
   } else {
     html += `
-      <h3 style="color:green;">🟢 Produit NON CMR</h3>
+      <h3 style="color:green;">🟢 PRODUIT NON CMR</h3>
       <p><b>${produit}</b></p>
-
-      <h4>Bonnes pratiques :</h4>
-      <ul>
-        <li>Port de gants adaptés</li>
-        <li>Aération suffisante</li>
-        <li>Lunettes si projections possibles</li>
-      </ul>
     `;
   }
 
-  html += `</div>`;
+  html += "</div>";
   document.getElementById("result").innerHTML = html;
 }
 
 function resetAnalyse() {
   selectedH = [];
-  document.querySelectorAll("button.selected").forEach(b => b.classList.remove("selected"));
+  document.querySelectorAll("button.selected").forEach(btn => btn.classList.remove("selected"));
   document.getElementById("produit").value = "";
   document.getElementById("result").innerHTML = "";
 }
-``
